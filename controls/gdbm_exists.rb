769a1da354db85b1c6e18875d5ce82be0a658560
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 16:57:30 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/gdbm_exists.rb
+++ controls/gdbm_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm gdbm exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'gdbm')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-gdbm_relative_path = input('command_path', value: '/bin/gdbmtool')
-gdbm_installation_directory = command("hab pkg path #{plan_ident}")
-gdbm_full_path = gdbm_installation_directory.stdout.strip + "#{ gdbm_relative_path}"
- 
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+command_relative_path = input('command_relative_path', value: '/bin/gdbmtool')
+command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+
 control 'core-plans-gdbm-exists' do
   impact 1.0
   title 'Ensure gdbm exists'
   desc '
-  '
-   describe file(gdbm_full_path) do
+  Verify gdbm by ensuring /bin/gdbm exists'
+  
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  describe file(command_full_path) do
     it { should exist }
   end
 end
