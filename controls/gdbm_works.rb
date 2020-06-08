90d0e4c4e1dc517ca3b059a1deb18b8fd33318e6
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 16:57:30 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/gdbm_works.rb
+++ controls/gdbm_works.rb
@@ -1,24 +1,28 @@
 title 'Tests to confirm gdbm works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'gdbm')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-gdbm-works' do
   impact 1.0
   title 'Ensure gdbm works as expected'
   desc '
+  Verify gdbm by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
-  gdbm_path = command("hab pkg path #{plan_ident}")
-  describe gdbm_path do
+  
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
-  gdbm_pkg_ident = ((gdbm_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  describe command("DEBUG=true; hab pkg exec #{ gdbm_pkg_ident} gdbmtool --version") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} gdbmtool --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /gdbmtool \(gdbm\) 1.18.1/ }
+    its('stdout') { should match /gdbmtool \(gdbm\) #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 end
